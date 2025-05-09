0755c11d63f56f2049db45299f23ba8d824cc4b2
Eric Park <122037200+ericpark11@users.noreply.github.com>
2025-05-09 19:52:54 -0400

    version2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,197 +1,103 @@
-'use client';
-import React, { useState,useEffect, useRef, createContext } from 'react';
-import Mailer from "./component/mailer";
-import Script from 'next/script';
-import * as gtag from './component/analytics';
-
-const ThemeContext = createContext(null);
+import Image from "next/image";
 
 export default function Home() {
-    //click Event
-    const [isOpen, setIsOpen] = useState(false)
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2 tracking-[-.01em]">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+              src/app/page.tsx
+            </code>
+            .
+          </li>
+          <li className="tracking-[-.01em]">
+            Save and see your changes instantly.
+          </li>
+        </ol>
 
-    const toggle = () => {
-        setIsOpen(!isOpen)
-    };
-    
-    //outside the SideBar
-    let menuRef = useRef<HTMLUListElement>(null);
-
-    useEffect(() =>{
-        let handler = (e : MouseEvent): void =>{
-            if(menuRef.current && !menuRef.current.contains(e.target as Node)){
-                setIsOpen(false);
-                console.log(menuRef.current);
-            }
-        };
-        document.addEventListener("mousedown", handler);
-
-        return() =>{
-            document.removeEventListener("mousedown", handler)
-        }
-    });
-
-    //Light Mode or Dark Mode
-    const [theme, setTheme] = useState("light");
-
-    const toggleTheme = () => {
-        setTheme((curr) => (curr === "light" ? "dark" : "light"));
-    };
-    return (
-
-        <ThemeContext.Provider value={{theme, toggleTheme} as any}>
-            <div className="App" id={theme}>
-                {/* <header>
-                    <title>Eric Park</title>
-                    <link rel="shortcut icon" type="x-icon" href="Images/logo.png"></link>
-                    <link rel='stylesheet' href="style.css"></link>
-                </header> */}
-                <main>
-                    
-                    <section className="header" id="header">
-                        <header>
-                        <Script src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GOOGLE_ANALYTICS_ID}`} />
-                            <Script id="gtag-init">
-                                {`
-                                window.dataLayer = window.dataLayer || [];
-                                function gtag(){dataLayer.push(arguments);}
-                                gtag('js', new Date());
-                                gtag('config', '${gtag.GOOGLE_ANALYTICS_ID}', {
-                                    page_path: window.location.pathname,
-                                });
-                                `}
-                        </Script>
-                        </header>
-                        <nav>
-                            <div className="navLinks">
-                                <ul className="sidebar" ref={menuRef} style={isOpen ? { display: 'flex' } : { display: 'none' }} >
-                                    <li><a href="#header">Home</a></li>
-                                    <li><a href="#info">About Me</a></li>
-                                    <li><a href="#like">What do I Like?</a></li>
-                                    <li><a href="#achi">Achievements</a></li>
-                                    <li><a href="#talk">Connect</a></li>
-                                </ul>
-                            
-                                <ul>
-                                    <li onClick={toggleTheme}><img src="Images/logo.png"></img><a href="#header"></a></li>
-                                    <li className="hideOnMobile"><a href="#info">About Me</a></li>
-                                    <li className="hideOnMobile"><a href="#like">What do I Like?</a></li>
-                                    <li className="hideOnMobile"><a href="#achi">Achievements</a></li>
-                                    <li className="hideOnMobile"><a href="#talk">Connect</a></li>
-                                    <li className="menuButton" onClick={toggle}><a href="#"><svg xmlns="http://www.w3.org/2000/svg" fill="white" height="26" viewBox="0 -960 960 960" width="26"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" /></svg></a></li>
-                                </ul>
-                            </div>   
-                        </nav>
-                        <div className="banner">
-                            <h1>ERIC PARK</h1>
-                        </div>
-                    </section>
-                
-
-                    <section className="info" id="info" >
-                        <h1>About Me</h1>
-                        <p>Hello! My name is Eric Park. I am a third-year Computer Science student at McMaster University. I am expected to graduate in 2027 with a co-op degree in B.A.Sc. Degree in Computer Science. I started my journey in Computer Science not too long ago in grade 11 in a high school course. I got into it thanks to my inspirational and well-respected Computer Science teacher from Leo Hayes High School. I got introduced to CS through Visual Basics and Java the year after. In my academic years at McMaster University, I learned languages such as Python, Haskell, Elm, and Bash and learned a little bit of Git/GitHub. Outside school content, I have learned LaTex, TypeScript, HTML, and CSS.</p>
-
-                        <div className="row">
-                            <div className="my-col">
-                                <h2>School</h2>
-                                <p>I am proud to say that I have been part of the Marauders since 2023 when I decided to move out from my hometown in New Brunswick to pursue my career as a Computer Scientist/Software Engineer at McMaster. It’s been a remarkable few semesters so far, and I cannot wait to learn and make more memories in this incredible community.</p>
-                            </div>
-                            <div className="my-col">
-                                <h2>Sports</h2>
-                                <p>Throughout my years, sports have always been a big part of me. From really any sports I will always be part of. The most recent sports team I participated in was my high school varsity soccer team. I like to push myself to my limits. My athletic years have taught me how to communicate with others and work in a team environment. </p>
-                            </div>
-                            {/* for three thing uncomment this  */}
-                            {/* <div className="my-col">
-                                <h2>Schools</h2>
-                                <p>I go to McMaster</p>
-                            </div> */} 
-                        </div>
-                    </section>
-                    <section className="like" id="like">
-                        <h1>What do I like?</h1>
-                        <p>when I like something I like it alot</p>
-
-                        <div className="row">
-                            <div className="like-col">
-                                <img src="Images/cars.jpg" />
-                                <h3>Cars</h3>
-                                <p> I have always been fascinated with the car world since I was young. My dream was to be a car designer, but my love for being a designer died down when I discovered the love of solving problems and coding instead of using my creative thinking skills. Designing this website brought me creative thinking skills I never thought I would use in this industry. It even made me fall more in love with front-end development!</p>
-                            </div>
-                            <div className="like-col">
-                                <img src="Images/coding.jpg" />
-                                <h3>Coding</h3>
-                                <p> I love problem-solving. It satisfies me after I solve such problems. Coding also always allows me to innovate and impact around me in various ways. The logical thinking that it take to be a good programmer makes me excel in this sector. I cannot wait to take this to another level where I can impact the community around me to help them with their problems as a computer scientist. </p>
-                            </div>
-                            <div className="like-col">
-                            <iframe src="https://open.spotify.com/embed/track/0FyRrCNUbl5Gy5HOuJW0aX?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-                                <h3>Music</h3>
-                                <p> I think everybody can agree on music. I love listening to music in cars. One of my favourite things is driving around the city with my friends. It has the power to evoke a wide range of emotions that can be a source of joy, comfort, nostalgia, or inspiration. Like my current favourite song above, your favourite music may be linked to specific memories or experiences, creating a solid emotional connection. </p>
-                            </div>
-                        </div>
-                    </section>
-
-                    <section className="achi" id="achi">
-                        <h1>My Achievements</h1>
-                        <p>I have been fortunate enough to receive several scholarships in my academic years; I have received the <i>Nashwaaksis Lion’s Club</i>, the <i>Coca-Cola Prize</i>, and <i>McMaster’s Award of Excellence</i>. I also have received Lion’s Principal Gold for achieving one of the highest grades in my class. I am truly humbled to say that I am continuing with a high GPA throughout my University days as well.  </p>
-
-                        <div className="row">
-                            <div className="achi-col">
-                                <img src="Images/Coca-Cola.jpg" />
-                                <div className="layer">
-                                    <h3>Coca-Cola Prize</h3>
-                                </div>
-                            </div>
-                            <div className="achi-col">
-                                <img src="Images/Nash.jpg" />
-                                <div className="layer">
-                                    <h3>Nashwaaksis Lion’s Club</h3>
-                                </div>
-                            </div>
-                            <div className="achi-col">
-                                <img src="Images/Excellence.jpg" />
-                                <div className="layer">
-                                    <h3>McMaster’s Award of Excellence</h3>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-
-
-
-                    <section className="talk" id="talk">
-                        <hr />
-                        <h1>Wanna Talk?</h1>
-
-
-                        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
-                        <div className="social-menu">
-                            <a href="https://www.instagram.com/eric.park04?igshid=YzVkODRmOTdmMw%3D%3D&utm_source=qr" className="insta" target="_blank"><i className="fab fa-instagram"></i></a>
-                            <a href="https://www.facebook.com/eric.park.5891?mibextid=LQQJ4d" className="fb" target="_blank"><i className="fab fa-facebook-f"></i></a>
-                            <a href="https://www.linkedin.com/in/eric-park-010665250?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" className="lkdn" target="_blank"><i className="fab fa-linkedin"></i></a>
-                            <a href="https://github.com/ericpark11" className="git" target="_blank"><i className="fab fa-github" ></i></a>
-                            <a href="mailto:park.ericyc@icloud.com" className="mail"><i className="fas fa-mail-bulk"></i></a>
-                        </div>
-
-                        <div className="comment-box">
-                            <Mailer/>
-                        </div>
-                    </section>
-
-
-                </main>
-
-                <footer>
-                    <p>Made By Eric Park</p>
-                </footer>
-            </div>
-        </ThemeContext.Provider>
-
-    )
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read our docs
+          </a>
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-
-
-
-
-
